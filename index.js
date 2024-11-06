5b2e4737abeef3c307e999a7127f39a1e9f357d5
nayeamseikhh <usanayeamseikh@gmail.com>
2024-11-06 08:43:02 +0600

    m

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 var cors = require('cors');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {res.send ('Thank You for Calling Me')});
 app.get('/fruit/banana', (req, res)=> {
@@ -26,6 +27,12 @@ app.get('/users/:id', (req, res)=>{
     const id = req.params.id;
     const name = users[id];
     res.send({name, id});
+});
+app.post('/addUser', (req, res)=>{
+    //save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
 })
 
 app.listen(4000, () => console.log('Listening to port 4000'))
